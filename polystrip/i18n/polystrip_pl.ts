9e768d5dddbaa5a73dd18cb8a02c15e45a13c163
Jeż Maryla (BG) <mjez@bzmw.gov.pl>
2026-06-03 11:36:11 +0200

    polish translation

---
+++ polystrip/i18n/polystrip_pl.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>PolyStrip</name>
+    <message>
+        <location filename="../polystrip.py" line="177"/>
+        <source>&amp;PolyStrip</source>
+        <translation>&amp;PolyStrip</translation>
+    </message>
+    <message>
+        <location filename="../polystrip.py" line="169"/>
+        <source>PolyStrip</source>
+        <translation>PolyStrip</translation>
+    </message>
+    <message>
+        <location filename="../polystrip.py" line="194"/>
+        <source>No layers with line features - polystrip needs a selected line feature!</source>
+        <translation>Brak warstwy liniowej - PolyStrip wymaga zaznaczenia obiektów liniowych!</translation>
+    </message>
+    <message>
+        <location filename="../polystrip.py" line="202"/>
+        <source>Select a Feature in a Vectorlayer</source>
+        <translation>Zaznacz obiekt na warstwie liniowej</translation>
+    </message>
+    <message>
+        <location filename="../polystrip.py" line="207"/>
+        <source>Select a Feature in a Linelayer</source>
+        <translation>Zaznacz obiekt na warstwie liniowej</translation>
+    </message>
+    <message>
+        <location filename="../polystrip.py" line="212"/>
+        <source>No feature in active Layer selected!</source>
+        <translation>Brak zaznaczonych obiektów na aktywnej warstwie!</translation>
+    </message>
+</context>
+<context>
+    <name>PolyStripDialog</name>
+    <message>
+        <location filename="../polystripdialog.ui" line="14"/>
+        <source>PolyStrip - Create Polygons along Lines</source>
+        <translation>PolyStrip - stwórz poligony wzdłuż linii</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="24"/>
+        <source>Layer Information</source>
+        <translation>Informacje o warstwie</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="30"/>
+        <source>Layer Unit:</source>
+        <translation>Jednostki warstwy:</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="50"/>
+        <source>Polygon Parameters</source>
+        <translation>Parametry poligonu</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="56"/>
+        <source>Polygon Width:</source>
+        <translation>Długość poligonu:</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="63"/>
+        <source>Width of each polygon strip along the line</source>
+        <translation>Długość każdego poligonu wzdłuż linii</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="82"/>
+        <source>Unit for width value</source>
+        <translation>Jednostka długości</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="89"/>
+        <source>Polygon Height:</source>
+        <translation>Wysokość poligonu:</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="96"/>
+        <source>Height (perpendicular distance) of each polygon from the line</source>
+        <translation>Wysokość każdego poligonu wzdłuż linii (prostopadła do linii)</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="115"/>
+        <source>Unit for height value</source>
+        <translation>Jednostka wysokości</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="125"/>
+        <source>Coverage Settings</source>
+        <translation>Ustawienia pokrycia</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="131"/>
+        <source>Overlap:</source>
+        <translation>Nakładanie:</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="138"/>
+        <source>Percentage of overlap between adjacent polygons</source>
+        <translation>Procent nakładania się sąsiadujących poligonów</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="141"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="154"/>
+        <source>Start Offset:</source>
+        <translation>Przesunięcie początkowe:</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="161"/>
+        <source>Distance from line start to begin creating polygons</source>
+        <translation>Odległość pomiędzy początkiem linii a pierwszym poligonem</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="180"/>
+        <source>Unit for start offset value</source>
+        <translation>Jednostka przesunięcia początkowego</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="190"/>
+        <source>Polygon Orientation</source>
+        <translation>Orientacja poligonów</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="196"/>
+        <source>Follow line direction</source>
+        <translation>Podążaj za kierunkiem linii</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="202"/>
+        <source>If checked, polygons will be rotated to follow the line direction. If unchecked, polygons will maintain north-up orientation.</source>
+        <translation>Jeśli zaznaczone, poligony zostaną obrócone zgodnie z kierunkiem linii. Jeśli odznaczone, poligony zostaną zorientowane w kierunku północnym.</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="212"/>
+        <source>Coordinate Reference System</source>
+        <translation>Układ współrzędnych</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="218"/>
+        <source>Use CRS from source layer (recommended)</source>
+        <translation>Użyj układu współrzędnych warstwy (zalecane)</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="224"/>
+        <source>Use the same coordinate reference system as the input line layer</source>
+        <translation>Użyj tego samego układu współrzędnych, co na wejściowej warstwie liniowej</translation>
+    </message>
+    <message>
+        <location filename="../polystripdialog.ui" line="231"/>
+        <source>Uncheck to select a different CRS for the output polygons</source>
+        <translation>Odznacz, aby wybrać inny układ współrzędnych dla wyjściowych poligonów</translation>
+    </message>
+</context>
+</TS>
